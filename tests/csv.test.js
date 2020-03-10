c23f7052a9f21f8ec3898c371c866e92d970a434
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-03-09 23:16:57 -0400

    Update csv.test.js

--- tests/csv.test.js
+++ tests/csv.test.js
@@ -1,6 +1,6 @@
 // CSV Reader Test
 const reader = require('../csvReader.js');
-const model = require('../Models/city')
+const model = require('../Models/city.js')
 
 test('Creating the model', () => {
     let city = new model();
